2fb10998537fa2caf216a2640ddcc9055b66e74e
Min1600 <aminhassan00@gmail.com>
2024-09-08 11:14:18 +0100

    update

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,8 +1,15 @@
 import "./styles.css";
-
+import {inbox} from "./inbox"
+import {today} from "./today"
 import {myProject} from "./myProject";
 
 const mainPage = () =>{
+const addTaskBtn = document.getElementById("addTask")
+const inboxBtn = document.getElementById("inbox")
+const todayBtn = document.getElementById("today")
+const projectBtn = document.getElementById("project")
+const upcomingBtn = document.getElementById("upcoming")
+
 
 }
 
