e9de18edc33c52301d0585c727b7a85d35a5f92a
Min1600 <aminhassan00@gmail.com>
2024-10-14 22:20:26 +0100

    ipdate

--- src/myProject.js
+++ src/myProject.js
@@ -1,49 +1,55 @@
-import {projectData, projectTaskData} from "./pageDOM"
+import { projectData, projectTaskData } from "./pageDOM";
 
+const myProject = () => {
+  const mainPage = document.getElementById("content");
+  const projectDiv = document.createElement("div");
+  const mainTitle = document.createElement("h1");
+  const projectBtn = document.getElementById("project");
+  const addTaskBtn = document.getElementById("addTask");
 
+  projectBtn.disabled = true;
+  projectDiv.className = "title";
+  addTaskBtn.disabled = true;
 
-const myProject = () =>{
-    const mainPage = document.getElementById("content")
-    const projectDiv = document.createElement("div")
-    const mainTitle = document.createElement("h1")
-    const projectBtn = document.getElementById("project")
-    const addTaskBtn = document.getElementById("addTask")
+  mainTitle.textContent = "My Projects";
 
+  mainPage.appendChild(projectDiv);
+  projectDiv.appendChild(mainTitle);
 
-    projectBtn.disabled = true;
-    projectDiv.className = "title"
-    addTaskBtn.disabled = true;
-
-
-    mainTitle.textContent = "My Projects"
-
-    mainPage.appendChild(projectDiv)
-    projectDiv.appendChild(mainTitle)
-
-    projectData()
-}
+  projectData();
+};
 
 const sideBarProjects = (id) => {
+  const mainPage = document.getElementById("content");
+  const projectDiv = document.createElement("div");
+  const title = document.createElement("h1");
+  const description = document.createElement("p");
+  let storageJSON = JSON.parse(localStorage.getItem("Storage")) || [];
+  let [,,, ...projects] = storageJSON
 
-    const mainPage = document.getElementById("content")
-    const projectDiv = document.createElement("div")
-    const title = document.createElement("h1");
-    const description = document.createElement("p");
-    let project = JSON.parse(localStorage.getItem("projectList")) || []
-    
-    project.forEach((item) => {if(item.title === id){
-        title.textContent = item.title;
-        description.textContent = item.description;
-      }}) 
+  projects.forEach((item) => {
+    if (item.id === id) {
+      title.textContent = item.title;
+      description.textContent = item.description;
+    }
+  });
 
+  projectDiv.className = "title";
+  mainPage.appendChild(projectDiv);
+  projectDiv.appendChild(title);
+  projectDiv.appendChild(description);
 
-      projectDiv.className = "title"
-      mainPage.appendChild(projectDiv)
-      projectDiv.appendChild(title)
-      projectDiv.appendChild(description)
+  projectTaskData();
+};
 
-      projectTaskData()
+function projectSidebar(obj) {
+  const projects = document.querySelector(".projectDiv");
+  const newProject = document.createElement("button");
+
+  newProject.textContent = obj.title;
+  newProject.className = "projectTab";
+  newProject.id = obj.id;
+  projects.appendChild(newProject);
 }
 
-
-export{myProject, sideBarProjects}
+export { myProject, sideBarProjects, projectSidebar };
